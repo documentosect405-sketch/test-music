12094128dcad9921f84c550586dc105b6acf9630
documentosect405-sketch <documentosect405@gmail.com>
2026-08-22 13:34:53 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 require('./main');
-require('./shiva'); 
+
 const path = require('path');
 const express = require("express");
 const app = express();
 const port = 8888;
+
+// Ruta principal (para que Render no apague el bot)
 app.get('/', (req, res) => {
     const imagePath = path.join(__dirname, 'index.html');
     res.sendFile(imagePath);
 });
+
+// Iniciar servidor web
 app.listen(port, () => {
-    console.log(`🔗 Listening to GlaceYT : http://localhost:${port}`);
+    console.log(`🔗 Bot activo en: http://localhost:${port}`);
 });
